05a6ef321e607beaa170c098b3d390978a5cf81a
ClaireGuerin <guerin.claire01@gmail.com>
2021-10-05 18:21:42 +0200

    installing snap only for ubuntu < 16

--- install_vscode.sh
+++ install_vscode.sh
@@ -1,2 +1,14 @@
-sudo apt update
-sudo apt install snapd
+#!/bin/sh
+# run from root
+
+MINVER=16
+CURVER=$(lsb_release -rs | grep -o -E '[0-9]+\.' | grep -o -E '[0-9]+')
+
+if [ $CURVER -ge $MINVER ];
+then
+    echo "Ubuntu version >= 16.04. No need to install snap" 
+else
+    echo "Ubuntu version < 16.04. Trying to install snap..."
+    apt update
+    apt install snapd -y
+fi
