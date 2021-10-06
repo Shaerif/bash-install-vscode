5111ef9b1b820881057ec41da43161710548718a
ClaireGuerin <guerin.claire01@gmail.com>
2021-10-06 10:19:15 +0200

    VS code extensions

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ Bash scripts to install Visual Studio Code and set up CMake on Ubuntu
 1. Download this repository onto your local machine. 
 2. Open your terminal and navigate to where the repository is with `cd path/to/repo/bash-install-vscode`
 3. Make sure the file `install_vscode.sh` is executable with `ls -al`. `x` should indicate execute mode. If not, make the file executable with `chmod +x install_vscode.sh`. ![make file executable](img/executable.png)
-4. Execute bash file in root with `sudo source install_vscode.sh`
+4. Execute bash file with `source install_vscode.sh`. You'll need admin rights and will be prompted for your password if not in root.
--- install_vscode.sh
+++ install_vscode.sh
@@ -18,3 +18,15 @@ fi
 # 2: Install Visual Studio Code with snap
 
 snap install --classic code
+# snap refresh code
+
+# 3: Install Visual Studio Code extensions for C++:
+# Clang, Syntax highlighting, CMake, etc.
+code --install-extension --force ms-vscode.cpptools # C++
+code --install-extension --force ms-vscode.cpptools-extension-pack # C++ popular extensions
+code --install-extension --force ms-vscode.cpptools-themes # Colorization
+code --install-extension --force jeff-hykin.better-cpp-syntax # Syntax highlighting
+code --install-extension --force ms-vscode.cmake-tools # CMake
+code --install-extension --force twxs.cmake # CMake
+code --install-extension --force notskm.clang-tidy # Clang
+code --install-extension --force alesiong.clang-tidy-linter # Clang linting
